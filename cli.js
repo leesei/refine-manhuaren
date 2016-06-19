852f962af54007946a532391dee5e135ef7dbc95
leesei <leesei@gmail.com>
2016-06-19 15:01:18 +0800

    refactor: use `cmds/` for subcommands
    report error and print help upon subcommand mismatch
    
     Changes to be committed:
    	new file:   cli.js
    	deleted:    cli/cli.js
    	renamed:    cli/extract.js -> cmds/extract.js
    	renamed:    cli/info.js -> cmds/info.js
    	modified:   package.json

---
+++ cli.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+require('yargs')
+  .usage('Usage: $0 <command> [options]')
+  .version()
+  .alias('v', 'version')
+  .help()
+  .alias('h', 'help')
+  .command(require('./cmds/info'))
+  .command(require('./cmds/extract'))
+  .example('$0 info ~/manhuaren/', 'list comics in folder')
+  .example('$0 extract ~/manhuaren/ ~/out -z', 'extract comics in ~/manhuaren/ as zip files in ~/out')
+  .demand(1)
+  .wrap(null)
+  .strict()
+  .argv;
